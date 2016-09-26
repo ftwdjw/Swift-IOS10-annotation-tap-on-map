6f4bd7ad031af733541bde7b7ae204559f6e45f3
ftwdjw <ftwdjw@gmail.com>
2016-09-26 11:42:45 -0500

    initial commit. tested on Xcode 8 IOS10

--- mapAnnotation/ViewController.swift
+++ mapAnnotation/ViewController.swift
@@ -13,26 +13,108 @@ import CoreLocation
 class ViewController: UIViewController, MKMapViewDelegate,
 CLLocationManagerDelegate {
     
+    //variables
+    var measurement=0
     var i=0
+    var latitude=0.0
+    var longitude=0.0
     
     
     @IBOutlet weak var mapView: MKMapView!
     
-    @IBAction func tapOnMap(_ sender: UITapGestureRecognizer) {
+    @IBAction func tapOnMap(_ gestureRecognizer: UITapGestureRecognizer) {
         i += 1
         print("this is a tap on the map number=\(i)")
+        print("latiutude=\(latitude)")
+        print("longitude=\(longitude)")
+        let touchPoint = gestureRecognizer.location(in: mapView)
+        let newCoordinates = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = newCoordinates
+        mapView.addAnnotation(annotation)
+        
     }
     
+    // MARK: - Properties
+    
+    lazy var locationManager : CLLocationManager = {
+        
+        let locationManager = CLLocationManager()
+        
+        //Set up location manager with defaults
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.delegate = self
+        
+        //Optimisation of battery
+        locationManager.pausesLocationUpdatesAutomatically = true
+        locationManager.activityType = CLActivityType.fitness
+        locationManager.allowsBackgroundLocationUpdates = false
+        
+        return locationManager
+    }()
+
+    
+    
+    
+    //let locationManager = CLLocationManager()
+    //initialize location mnanager
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.requestAlwaysAuthorization()
+        
+        self.locationManager.startUpdatingLocation()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - CLLocation didUpdateLocations
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations
+        locations: [CLLocation])
+    {
+        let location = locations.last
+        
+        print("update last location")
+        
+        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        latitude=location!.coordinate.latitude
+        longitude=location!.coordinate.longitude
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        self.mapView.setRegion(region, animated: true)
+        
+        //self.locationManager.stopUpdatingLocation()
+    }
+    
+    // MARK: - CLLocationManagerDelegate
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if status == CLAuthorizationStatus.authorizedAlways {
+            mapView.showsUserLocation = true
+            mapView.userTrackingMode = MKUserTrackingMode.follow
+            
+            print("\nPermission Allowed\n")
+        } else {
+            print("\nPermission Refused\n")
+        }
+    }
+    
+    // MARK: - LocationManagerError
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error:Error)
+    {
+        print("Error: " + error.localizedDescription)
+    }
+
 
 
 }
